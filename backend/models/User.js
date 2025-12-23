32fe41d9191e62d7ee3b770dca4ac6b6993a3043
Reva <reva.raspaile.s73@kalvium.community>
2025-12-24 00:05:57 +0530

    lil change 6.0

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -27,5 +28,17 @@ const userSchema = new mongoose.Schema({
     default: "student"
   }
 });
+
+// Hash password before saving
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Method to compare plain password with hashed password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
