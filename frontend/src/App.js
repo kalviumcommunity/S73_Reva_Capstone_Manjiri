e77ccd06fa0429a8bb92b937d36d53e04e2d6c7c
Reva <reva.raspaile.s73@kalvium.community>
2025-12-24 16:14:31 +0530

    Initialized react/frontend application

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Manjiri - School Management System</h1>
+      <p>Welcome to the smart solution for CBSE principals</p>
+    </div>
+  );
+}
+
+export default App;
